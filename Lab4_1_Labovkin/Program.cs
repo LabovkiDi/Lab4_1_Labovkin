d2b7b19a8ec2c763c374540c3e651d0837540520
Admin <Admin@HP>
2021-12-26 23:20:17 +0300

    Добавьте файлы проекта.

---
+++ Lab4_1_Labovkin/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_1_Labovkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, s1; string s;
+            Console.WriteLine("Введите целое число(N>0) N=");
+            s = Console.ReadLine();
+            N = Convert.ToInt32(s);
+            Console.WriteLine("Квадраты чисел от 1 до N:");
+            s1 = 0;
+            for (int i = 1; i <= (2 * N - 1); i = i + 2)
+            {
+                s1 += i;
+                Console.Write("{0},", s1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
